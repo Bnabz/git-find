9cd76725c0ba5a9832a4e2f21ea2d6fc10b8eb86
Nabz <bnnabiswa@gmail.com>
2020-04-08 21:40:07 +0300

    create repository class

---
+++ src/app/repository.ts
@@ -0,0 +1,10 @@
+export class Repository {
+  constructor(public name: string,
+            public description: string,
+            public url: string,
+            public language: string,
+            public forks:number,
+            public watchers:number
+            public updated_on: Date) {}
+
+}

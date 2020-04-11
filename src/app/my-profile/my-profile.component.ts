5f6559c50a9943ff9842700ab28c4007af40ea6c
Nabz <bnnabiswa@gmail.com>
2020-04-11 04:52:40 +0300

    install bootstrap

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService }from '../profile-service/profile.service';
 
 @Component({
   selector: 'app-my-profile',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-profile.component.css']
 })
 export class MyProfileComponent implements OnInit {
+  profile:any;
+  repos:any;
+  username:string;
 
-  constructor() { }
+  constructor(private profileService:ProfileService){
+  this.profileService.updateProfile(this.username);
+  this.profileService.getProfileInfo().subscribe(profile => {
+    this.profile = profile;
+  });
+  this.profileService.getProfileRepos().subscribe(repos => {
+     this.repos = repos;
+   });
+
+  }
+
+
 
   ngOnInit(): void {
+
+    this.profileService.updateProfile('Bnabz');
+    this.profileService.getProfileInfo().subscribe(profile => this.profile = profile);
+    this.profileService.getProfileRepos().subscribe(repos =>  this.repos = repos);
+
   }
 
 }
--- src/app/profile-service/profile.service.ts
+++ src/app/profile-service/profile.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient }from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username:string;
+ private client_id = "Iv1.ca62bfff5ab5f94f";
+  private client_secret ="6cbf4042e7336ee6bfca47a4f0988a70e90ccd9c";
+
+    constructor(private http:HttpClient) {
+    this.username = "Bnabz";
+  }
+    getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.client_id + "&client_secret=" + this.client_secret)
+  }
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id="+ this.client_id + "&client_secret=" + this.client_secret)
+  }
+  updateProfile(username:string){
+    this.username = username;
+  }
+
 }
+//092ac5ce8ff7c9ab18fa2cd7e9404d3829104402
+//Client ID: Iv1.ca62bfff5ab5f94f
+//Client secret: 6cbf4042e7336ee6bfca47a4f0988a70e90ccd9c
+//Private key 1f:90:21:12:34:24:17:12:8c:1f:d3:20:b9:6f:27:ed:3c:33:15:47

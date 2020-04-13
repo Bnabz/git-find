7ceec0f38c94649cafc68e2aa336d8538c1b7609
Nabz <bnnabiswa@gmail.com>
2020-04-13 11:56:02 +0300

    add routing functionality

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from '../not-found/not-found.component';
+import { MyProfileComponent } from '../my-profile/my-profile.component';
+import { RepoSearchComponent } from '../repo-search/repo-search.component'
 
+const routes: Routes = [
+  { path: 'home', component: MyProfileComponent},
+{ path: 'repos', component: RepoSearchComponent},
+{ path: '', redirectTo:"/goals", pathMatch:"full"}
+ { path:'**', component:NotFoundComponent},
 
+]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule],
+  declarations: []
 })
 export class RoutingModule { }
